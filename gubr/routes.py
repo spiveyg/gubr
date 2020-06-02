ed96daff96ef3ffc8ec078f0f63eb4fe8c01738a
spiveyg <spiveyg@gmail.com>
2020-06-02 09:08:17 -0700

    modified code to make login more robust

--- gubr/routes.py
+++ gubr/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import (render_template, flash, redirect,)
 from gubr.forms import LoginForm
 from gubr import app
 
@@ -24,7 +24,12 @@ def home():
 
 
 # login route - 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f'Login requested for user {form.username.data}, \
+            remember_me={form.remember_me.data}')
+        return redirect('/home')
     return render_template('login.html', title='Sign In', form=form)
